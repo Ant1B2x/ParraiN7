d04216b584cffc3a8e13297619b568670e008f02
Seaziicks <esteban.baichoo@gmail.com>
2020-10-29 10:28:42 +0100

    Creating rankings service

---
+++ server/src/models/rankings.model.ts
@@ -0,0 +1,27 @@
+// rankings-model.ts - A KnexJS
+//
+// See http://knexjs.org/
+// for more of what you can do here.
+import Knex from 'knex';
+import {Application} from '../declarations';
+
+export default function (app: Application): Knex {
+    const db: Knex = app.get('knexClient');
+    const tableName = 'rankings';
+    db.schema.hasTable(tableName).then(exists => {
+        if (!exists) {
+            db.schema.createTable(tableName, table => {
+                table.uuid('godfatherId').references('id').inTable('users');
+                table.uuid('godsonId').references('id').inTable('users');
+                table.primary(['godfatherId', 'godsonId']);
+
+                table.integer('rank').notNullable();
+            })
+                .then(() => console.log(`Created ${tableName} table`))
+                .catch(e => console.error(`Error creating ${tableName} table`, e));
+        }
+    });
+
+
+    return db;
+}
