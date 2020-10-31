f280d33f880cbafebc812bbd479657835a3ad894
Esteban Baichoo <esteban.baichoo@gmail.com>
2020-10-31 11:05:50 +0100

    Adding label to avoid warnings.

--- client/src/views/Questions.vue
+++ client/src/views/Questions.vue
@@ -7,7 +7,9 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="basic-addon1">?</span>
                     </div>
-                    <input type="text" class="form-control" placeholder="Votre question">
+                    <label>
+                        <input type="text" class="form-control" placeholder="Votre question">
+                    </label>
                     <div class="input-group-append">
                         <span class="input-group-text" id="basic-addon2">¿</span>
                     </div>
