9b66da4011ee049db460091402ac7c2a5d0819c1
Ant1B2x <antoine.bedex@tutanota.com>
2020-11-03 17:48:57 +0100

    change check-setting-admin-user hook description and error

--- server/src/hooks/users/check-setting-admin-user.ts
+++ server/src/hooks/users/check-setting-admin-user.ts
@@ -4,15 +4,14 @@ import {Hook, HookContext} from '@feathersjs/feathers';
 import {UserData} from '../../services/users/users.class';
 import {Forbidden} from "@feathersjs/errors";
 
-// check that an user could only be set admin by an admin
+// ensure that a non-admin user cannot set himself as admin
 // warning : has to be included AFTER authenticate hook
 export default (options = {}): Hook => {
     return async (context: HookContext): Promise<HookContext> => {
         const newData: UserData = context.data;
-        const currentUser: UserData = await context.app.service('users').get(context.id);
         const loggedUser: UserData = context.params.user;
         if (!loggedUser.isAdmin && newData['isAdmin'])
-            throw new Forbidden(`User ${loggedUser.email} can't set ${currentUser.email} as admin!`);
+            throw new Forbidden(`User ${loggedUser.email} can't set himself as admin!`);
 
         return context;
     };
