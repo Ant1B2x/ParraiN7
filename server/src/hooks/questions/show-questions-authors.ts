9878c4be719520d35d085a958ae7cbcab74d46f9
Ant1B2x <antoine.bedex@tutanota.com>
2020-11-04 00:04:27 +0100

    show answers users hook

--- server/src/hooks/questions/show-questions-authors.ts
+++ server/src/hooks/questions/show-questions-authors.ts
@@ -1,8 +1,6 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 import {Hook, HookContext} from '@feathersjs/feathers';
-import {checkAdmin} from '../common-checks';
-import {checkGodfather} from '../common-checks';
 import {UserData} from "../../services/users/users.class";
 
 // add authors names to sent questions
