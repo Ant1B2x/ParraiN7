9654f6bb10cb8e8fb5ac58f981c3a960b0d90717
Ant1B2x <antoine.bedex@tutanota.com>
2020-11-09 21:39:59 +0100

    you can use if instead of try catch

--- server/src/hooks/rankings/create-if-not-exists.ts
+++ server/src/hooks/rankings/create-if-not-exists.ts
@@ -1,28 +1,29 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 import {Hook, HookContext} from '@feathersjs/feathers';
+import {NotAcceptable} from "@feathersjs/errors";
 
 // create the rank if it's unexistant
 // warning : has to be included AFTER authenticate hook
 export default (options = {}): Hook => {
     return async (context: HookContext): Promise<HookContext> => {
-        try {
-            const result = await context.app.service('rankings').find({
-                query: {
-                    godfatherId: context.params.user.id,
-                    godsonId: context.data.godsonId
-                },
-                user: context.params.user
-            });
-            if (result.length === 0)
-                throw new Error('Attention !');
-            context.id = result[0]['id']
-        } catch (err) {
+        if (!context.data['godsonId'])
+            throw new NotAcceptable('You have to provide a godsonId!');
+
+        const result = await context.app.service('rankings').find({
+            query: {
+                godfatherId: context.params.user.id,
+                godsonId: context.data.godsonId
+            },
+            user: context.params.user
+        });
+        if (result.length === 0) {
             const result = await context.app.service('rankings').create(context.data, {
                 user: context.params.user
             });
-            // context.data['id'] = result['id'];
-            context.id = result['id']
+            context.id = result['id'];
+        } else {
+            context.id = result[0]['id']
         }
 
         return context;
