8e5e76b82fc5c4f9a08375617cce0c24332a0152
Esteban Baichoo <esteban.baichoo@gmail.com>
2020-11-09 20:23:00 +0100

    Making things work.

--- server/src/hooks/rankings/create-if-not-exists.ts
+++ server/src/hooks/rankings/create-if-not-exists.ts
@@ -1,18 +1,22 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 import {Hook, HookContext} from '@feathersjs/feathers';
-import {RankingData} from '../../services/rankings/rankings.class';
-import {UserData} from '../../services/users/users.class';
-import {Conflict} from "@feathersjs/errors";
 
 // create the rank if it's unexistant
 // warning : has to be included AFTER authenticate hook
 export default (options = {}): Hook => {
     return async (context: HookContext): Promise<HookContext> => {
         try {
-            await context.app.service('rankings').get(context.id, {
+            const result = await context.app.service('rankings').find({
+                query: {
+                    godfatherId: context.params.user.id,
+                    godsonId: context.data.godsonId
+                },
                 user: context.params.user
             });
+            if (result.length === 0)
+                throw new Error('Attention !');
+            context.id = result[0]['id']
         } catch (err) {
             const result = await context.app.service('rankings').create(context.data, {
                 user: context.params.user
