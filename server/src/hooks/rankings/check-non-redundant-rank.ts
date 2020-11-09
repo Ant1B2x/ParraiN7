85f9708133ad1626a577581231c19a9a07e2b14d
Ant1B2x <antoine.bedex@tutanota.com>
2020-11-09 21:46:11 +0100

    corrected check non redundant rank hook

--- server/src/hooks/rankings/check-non-redundant-rank.ts
+++ server/src/hooks/rankings/check-non-redundant-rank.ts
@@ -3,13 +3,17 @@
 import {Hook, HookContext} from '@feathersjs/feathers';
 import {RankingData} from '../../services/rankings/rankings.class';
 import {UserData} from '../../services/users/users.class';
-import {Conflict} from "@feathersjs/errors";
+import {Conflict, NotAcceptable} from "@feathersjs/errors";
 import {log} from "winston";
 
 // check that the provided rank is not redundant (a godfather shouldn't give the same rank to 2 godsons)
 // warning : has to be included AFTER authenticate hook
 export default (options = {}): Hook => {
     return async (context: HookContext): Promise<HookContext> => {
+        const godsonId: number = context.data['godsonId'];
+        if (!godsonId)
+            throw new NotAcceptable('You have to provide a godsonId!');
+
         const rank: number = context.data['rank'];
         if (rank) {
             const loggedUser: UserData = context.params.user;
@@ -20,7 +24,7 @@ export default (options = {}): Hook => {
                 },
                 user: loggedUser
             });
-            if (rankings.length)
+            if (rankings.length && rankings[0].godsonId !== godsonId)
                 throw new Conflict(`User ${loggedUser.id} already gaved rank ${rank} to someone!`);
         }
 
