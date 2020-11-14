a128e220f7cf68d804473ab0c72013138f3b2daf
Ant1B2x <antoine.bedex@tutanota.com>
2020-11-15 00:47:27 +0100

    make client contact production server and disable useless socketio cookie

--- client/src/feathers-client.ts
+++ client/src/feathers-client.ts
@@ -1,10 +1,12 @@
 
+import BACKEND_URL from '@/config';
+
 import feathers from '@feathersjs/feathers';
 import auth from '@feathersjs/authentication-client';
 import socketio from '@feathersjs/socketio-client';
+import io from 'socket.io-client';
 
-import socket from '@/socket-config';
-
+const socket = io(BACKEND_URL);
 const app = feathers();
 app.configure(socketio(socket));
 app.configure(auth({
