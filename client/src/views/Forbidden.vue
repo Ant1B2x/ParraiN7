8c57ab95c532bd2c0fbb196b360aac7c0c1a7b57
Esteban Baichoo <esteban.baichoo@gmail.com>
2020-12-13 18:17:36 +0100

    Adding some routes, and a Forbidden page.

---
+++ client/src/views/Forbidden.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>403</h1>
+        <p>Accès interdit</p>
+    </div>
+</template>
+
+<style scoped>
+@import "css/Forbidden.css";
+</style>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+
+@Component
+export default class Forbidden extends Vue {
+
+}
+
+</script>
+
