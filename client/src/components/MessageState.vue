5d63d8d30d3e39d6a18a44f16db2926189a96cab
Esteban Baichoo <esteban.baichoo@gmail.com>
2020-12-19 17:16:31 +0100

    くだらない。...

--- client/src/components/MessageState.vue
+++ client/src/components/MessageState.vue
@@ -1,10 +1,8 @@
 <template>
     <div class="alertify-notifier ajs-bottom ajs-right">
         <div v-for="(notification, index) in notifications"  :key="index" class="notification" :class="[notification.state, notification.step]">
-            <div class="d-flex px-3 py-2">
-                <div class="pr-2">
-                    <i class="fa fa-check" aria-hidden="true"></i>
-                </div>
+            <div class="d-flex px-3 py-2 align-items-center">
+                    <span class="icon"><font-awesome-icon :icon="notification.icon"/></span>
                 <div class="alert-text-body" role="status">{{notification.message}}</div>
             </div>
         </div>
@@ -29,11 +27,13 @@ class Notification {
     message: string;
     state: States;
     step: NotificationStep;
+    icon: string;
 
-    constructor(message: string, state: States, step: NotificationStep) {
+    constructor(message: string, state: States, step: NotificationStep, icon: string) {
         this.message = message;
         this.state = state;
         this.step = step;
+        this.icon = icon;
     }
 }
 
@@ -43,25 +43,23 @@ export default class MessageState extends Vue {
     notifications: Notification[] = [];
 
     displaySuccess(message: string) {
-        this.displayNotification(message, States.hasSucceed)
+        this.displayNotification(message, States.hasSucceed, 'check')
     }
 
     displayWarning(message: string) {
-        this.displayNotification(message, States.hasWarning)
+        this.displayNotification(message, States.hasWarning, 'exclamation-triangle')
     }
 
     displayError(message: string) {
-        this.displayNotification(message, States.hasError)
+        this.displayNotification(message, States.hasError, 'biohazard');
     }
 
-    displayNotification(message: string, state: States) {
-        this.notifications.push(new Notification(message, state, NotificationStep.in));
+    displayNotification(message: string, state: States, icon: string) {
+        this.notifications.push(new Notification(message, state, NotificationStep.in, icon));
         setTimeout(() => {
-            this.notifications[0].step = NotificationStep.out;
-            setTimeout(() => {
-                this.notifications.shift();
-            }, 985);
-        }, 6750);
+            this.notifications.shift();
+            console.log('plap');
+        }, 6000);
     }
 
     private getLastNotification() {
@@ -78,6 +76,19 @@ export default class MessageState extends Vue {
 
     public isOnWarning() {
         return this.getLastNotification().state === States.hasWarning;
+    }
+
+    public getIconFromState(state: States) {
+        switch (state) {
+            case States.hasSucceed:
+                return 'check';
+            case States.hasError:
+                return 'exclamation-triangle';
+            case States.hasWarning:
+                return 'exclamation-triangle';
+            case States.none:
+                return 'user-ninja';
+        }
     }
 }
 
