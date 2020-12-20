4a2d9873f2fde03b799c00a5cc5d982bb42220fc
Ant1B2x <antoine.bedex@tutanota.com>
2020-12-20 14:27:46 +0100

    check validated email was wrong

--- server/src/hooks/check-validated-email.ts
+++ server/src/hooks/check-validated-email.ts
@@ -6,7 +6,7 @@ import {MethodNotAllowed} from "@feathersjs/errors";
 // check if user's email has been validated by email
 export default (options = {}): Hook => {
     return async (context: HookContext): Promise<HookContext> => {
-        const tokens = context.app.service('tokens').find({
+        const tokens = await context.app.service('tokens').find({
             query: {
                 userId: context.params.user.id
             }
